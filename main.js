ff1c53fcf7d325f0f1a31fbd5d3d00b764075aff
Joey <josephgroark@gmail.com>
2022-02-06 08:19:30 +0000

    Disabled button on outOfStock

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
 const app = Vue.createApp({
   data() {
     return {
-      product: "Socks",
+      product: "Joey's sourbread",
+      image: "./assets/images/boule.jpg",
+      inventory: 0,
+      cart: 0,
+      freshToday: true,
+      ingredients: ["Flour", "Water", "Starter", "Salt"],
+      varieties: [
+        { id: 1, style: "boule", name: "Joey's sourbread" },
+        { id: 2, style: "bloomer", name: "San Francisco sourbread" },
+      ],
     };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    decreaseCart() {
+      this.cart -= 1;
+    },
+    updateImage(varietyImage) {
+      this.image = `./assets/images/${varietyImage}.jpg`;
+    },
+  },
+  computed: {
+    isOutOfStock() {
+      if (this.inventory <= 0) {
+        return true;
+      }
+    },
   },
 });
